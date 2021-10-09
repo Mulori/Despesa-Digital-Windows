b79005e7d41e7c056ab25681d48729d769375ced
mulori <murilogarcia.hr@gmail.com>
2021-10-09 17:22:38 -0300

    Criação de modal para vinculação de setores e fornecedores no produto, ja com o cache de seleção para não limpar o checkboxlist toda vez que seleciona um registro diferente

---
+++ Code/DTO/dtoModal/dtoModalCheckListBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DespesaDigital.Code.DTO.dtoModal
+{
+    public class dtoModalCheckListBox
+    {
+        public int codigo { get; set; }
+        public string descricao { get; set; }
+    }
+}
