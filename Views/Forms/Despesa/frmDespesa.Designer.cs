1fb10d9d16ca7dab99c068ec258c5f4a7b67568f
mulori <murilogarcia.hr@gmail.com>
2021-10-23 12:36:12 -0300

    Implemetnacao de visualizacao de imagem

--- Views/Forms/Despesa/frmDespesa.Designer.cs
+++ Views/Forms/Despesa/frmDespesa.Designer.cs
@@ -46,9 +46,9 @@ namespace DespesaDigital.Views.Forms.Despesa
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(128, 34);
+            this.pictureBox1.Location = new System.Drawing.Point(143, 243);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(860, 516);
+            this.pictureBox1.Size = new System.Drawing.Size(328, 230);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
